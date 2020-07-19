75ce6e04014dea3141ed867440c502e4cdd6959a
ishaan-k03 <25415242+ishaan-k03@users.noreply.github.com>
2020-07-19 23:24:32 +0530

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import models, routes
