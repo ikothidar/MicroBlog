fe303ec4352b37b43475b63cd77e070bcd66837f
ishaan-k03 <25415242+ishaan-k03@users.noreply.github.com>
2020-07-20 23:34:36 +0530

    commit

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
