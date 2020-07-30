4a283c3670f1bb0705f47b6408e20ec5d5548c5f
Ishaan Kothidar <ishaan.kothidar@ad.infosys.com>
2020-07-30 22:40:22 +0530

    commit

--- app/forms.py
+++ app/forms.py
@@ -55,3 +55,13 @@ class EmptyForm(FlaskForm):
 class PostForm(FlaskForm):
     post = TextAreaField("Say Something", validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField("Submit")
+
+class ResetPasswordRequestFrom(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Request Password Reset')
